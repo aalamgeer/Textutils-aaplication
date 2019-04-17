c427131c12ab2d1192aa0ff59522dd6d7008c4cc
aalamgeer <ranaaalamgeer92@gmail.com>
2019-04-17 21:32:54 +0530

    analyzer text with bootstrap home page

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -8,12 +8,39 @@ def index(request):
 
 
 def analyzer(request):
-    print(request.GET.get('text'))
-    data = request.GET.get('text')
-    removepunc = request.POST.get('removepunc', 'off')
-    analyzed = data
-    context = {'purpose': 'Remove Punctuations', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', context)
+    data = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    capitalize = request.GET.get('capitalize', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremove = request.GET.get('newlineremove', 'off')
+    analyzed = ''
+    count = 0
+    for chr in data:
+        if chr == 'a':
+            count += 1
+    if removepunc == 'on':
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for chr in data:
+            if chr not in punctuations:
+                analyzed = analyzed + chr
+        context = {'purpose': 'Remove Punctuations', 'analyzed_text': analyzed, 'no_of_a': count}
+        return render(request, 'analyze.html', context)
+    elif capitalize == 'on':
+        analyzed = data.capitalize()
+        context = {'purpose': 'Capitalize first', 'analyzed_text': analyzed, 'no_of_a': count}
+        return render(request, 'analyze.html', context)
+    elif fullcaps == 'on':
+        analyzed = data.upper()
+        context = {'purpose': 'Capitalize String', 'analyzed_text': analyzed, 'no_of_a': count}
+        return render(request, 'analyze.html', context)
+    elif newlineremove == 'on':
+        for chr in data:
+            if chr != '\n':
+                analyzed = analyzed + chr
+        context = {'purpose': 'New line remover', 'analyzed_text': analyzed, 'no_of_a': count}
+        return render(request, 'analyze.html', context)
+    else:
+        return HttpResponse('Error')
 
 
 '''def capitalize(request):
