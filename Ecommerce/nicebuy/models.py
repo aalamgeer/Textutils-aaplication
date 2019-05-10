476c8cc86446bfa8a21959e116f36bbcf16e7c55
aalamgeer <ranaaalamgeer92@gmail.com>
2019-05-11 01:22:13 +0530

    Design product models

--- Ecommerce/nicebuy/models.py
+++ Ecommerce/nicebuy/models.py
@@ -7,7 +7,10 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50, null=True)
     category = models.CharField(max_length=50, null=True)
     sub_category = models.CharField(max_length=50, null=True)
-    price = models.CharField(max_length=20, null=True)
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300, null=True)
-    pub_date = models.DateField()
-    image = models.CharField(max_length=50, null=True)
+    pub_date = models.DateField(null=True)
+    image = models.ImageField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.product_name
